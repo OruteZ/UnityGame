988bfc662302a6468506724d58293132b96a6e3a
OruteZ <47718603+OruteZ@users.noreply.github.com>
2021-05-31 16:15:23 +0900

    전투 일부분 및 메트로놈 구현

--- Assets/scripts/MonsterMove.cs
+++ Assets/scripts/MonsterMove.cs
@@ -7,17 +7,26 @@ public class MonsterMove : MonoBehaviour
     Rigidbody2D rigid;
     public int nextMove;
     public Transform player;
+    Metronome rhythem;
 
     void Start()
     {
+        rhythem = GameObject.Find("Metronome").GetComponent<Metronome>();
         rigid = GetComponent<Rigidbody2D>();
         Think();
     }
-
+    
     // Update is called once per frame
     void FixedUpdate()
     {
-        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+        if (Vector3.Distance(player.position, transform.position) < 8f && rhythem.metronom == 2)
+        {
+            alert();
+            Invoke("attack", 60 / rhythem.bpm);            
+        }
+        else
+            rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+
 
     }
 
@@ -31,5 +40,16 @@ public class MonsterMove : MonoBehaviour
         else nextMove = Random.Range(-1, 2);
 
         Invoke("Think", 2);
+    }
+
+    //공격하기 한 박자 전, 머리 위에 물음표를 띄우는 행위
+    void alert()
+    {
+        //스프라이트 구하는 대로 애니메이션 집어넣기
+    }
+    
+    void attack()
+    {
+
     }
 }
--- Assets/scripts/PlayerFight.cs
+++ Assets/scripts/PlayerFight.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class PlayerFight : MonoBehaviour
 {
+
+    Metronome rhythem;
+    Animator anim;
+    public bool isCounterOn = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        rhythem = GameObject.Find("Metronome").GetComponent<Metronome>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+            //rhythem = GameObject.Find("Metronome").GetComponent<Metronome>();
+            if (rhythem.accuracy > 0) isCounterOn = true;
+        if (rhythem.accuracy <= 0 && isCounterOn) isCounterOn = false;
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+    void Counter()
+    {
+        //공격하는 캐릭터 크기의 모션 따로 공수할 예정. 해당 스프라이트가 나오고 결정
+        Debug.Log("공격성공");
+    }
+    void Damaged()
+    {
+        Debug.Log("공격받음");
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "mob")
+        {
+            bool isMobAttacking = col.gameObject.GetComponent<Animator>().GetBool("isAttacking"); 
+            if(isMobAttacking)
+            {
+                if (isCounterOn) Counter();
+                else if (rhythem.accuracy <= 0) Damaged();
+            }
+        }
     }
 }
